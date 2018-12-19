3d233e31896ce613d37600cbab4f1ef40bffa93b
Diogo M de Oliveira <dmarostega@gmail.com>
2018-12-19 03:45:27 -0200

    Ajustes no evento do botão, marcar GOL

--- src/components/ButtonGol.js
+++ src/components/ButtonGol.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export default class ButtonGol extends React.Component{
+            /*por padrão função recebe parametro event */
+    handleClick(event){
+        /*Desabilita função padrão do botão*/
+        event.preventDefault();
+        this.props.setGol();
+    }
     render(){
-        return (<button onClick={this.props.setGol}>Goool</button>)
+          return (<button 
+                    onClick={this.handleClick.bind(this)}>
+                    Goool
+                </button>)    
+      /*ASSIM TAMBÉM FUNCIONOU 
+      
+        return (<button onClick={this.props.setGol}>Goool</button>)*/
     }
 }
